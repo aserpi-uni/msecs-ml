c273c2a87827e0884dc5faea6ca911766ea35107
Alessandro Serpi <alessandro.serpi@me.com>
2018-10-30 16:58:35 +0100

    Add family classification backbone

--- hw1/__main__.py
+++ hw1/__main__.py
@@ -1,4 +1,40 @@
 import argparse
+import json
+
+from sklearn.preprocessing import MultiLabelBinarizer
+
+from hw1.datasets import family_dataframe, feature_dataframe, one_hot_encode
+from hw1.evaluation import evaluate, print_heatmap
+
+
+def family_classification(directory, directory_results, algorithms, keys):
+    # Get a DataFrame containing only malwares in large families
+    families = family_dataframe(directory).groupby("family").filter(lambda f: len(f) > 20) \
+        .set_index("sha256").sort_index()
+
+    features = feature_dataframe(families.index.values, directory)
+    if keys:
+        features = features[keys]
+
+    # Replace NaNs with empty list
+    for col in features:
+        for row in features.loc[features[col].isna(), col].index:
+            features[col][row] = []
+
+    # Preprocess datasets
+    labels = families.family.unique()
+    X = one_hot_encode(MultiLabelBinarizer(), features, features.columns)
+    y = families.family.ravel()
+
+    scores, confusion_matrices = evaluate(X, y, algorithms)
+
+    # Save scores
+    with open("{}/scores".format(directory_results), "w") as fout:
+        json.dump(scores, fout, indent=4, sort_keys=True)
+
+    # Print confusion matrices
+    for alg in scores:
+        print_heatmap(confusion_matrices[alg], alg, directory_results, labels)
 
 
 if __name__ == "__main__":
