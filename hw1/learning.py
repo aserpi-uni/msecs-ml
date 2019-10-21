91e284846ad9376e556c3904d78d0c6714aa7ab5
Alessandro Serpi <alessandro.serpi@me.com>
2019-10-21 09:49:31 +0200

    Add possibility to stop and resume learning for Naive Bayes

--- hw1/learning.py
+++ hw1/learning.py
@@ -1,6 +1,6 @@
-import math
 import pandas as pd
 import pickle
+import os
 
 from hw1.classifiers import classifier
 
@@ -13,17 +13,34 @@ def learn(X, y, algs, out):
         clf = classifier(alg)
 
         # X is too big to fit in memory: partial fits
-        if len(X) * len(X.columns) > 50000:
-            iterations = math.ceil(len(X) / 5000)
+        if alg == "bernoulli" and len(X) * len(X.columns) > 350000000:
+            try:
+                with open(f"{out}/{alg}_batch.zip", "rb") as fin_batch, \
+                        open(f"{out}/{alg}.zip", "rb") as fin_classifier:
+                    (batches, first_batch) = pickle.load(fin_batch)
+                    clf = pickle.load(fin_classifier)
+            except (FileNotFoundError, TypeError):
+                batches = int(len(X) / 645)
+                first_batch = 0
             print("")
-            for i in range(0, len(y), 5000):
-                print(f"Fitting partition {i + 1} of {iterations}...", end="\t")
-                clf.partial_fit(X[i:i+4999], y[i:i+4999], classes=classes)
+
+            for i in range(first_batch, batches):
+                start_idx = int(len(X) / batches * i)
+                end_idx = int(len(X) / batches * (i + 1)) - 1
+                print(f"Fitting partition {i + 1} of {batches}...", end="\t")
+                clf.partial_fit(X[start_idx:end_idx], y[start_idx:end_idx], classes=classes)
+
+                with open(f"{out}/{alg}.zip", "wb") as fout_classifier:
+                    pickle.dump(clf, fout_classifier, protocol=pickle.HIGHEST_PROTOCOL)
+                with open(f"{out}/{alg}_batch.zip", "wb") as fout_batch:
+                    pickle.dump((batches, i + 1), fout_batch, protocol=pickle.HIGHEST_PROTOCOL)
                 print("Done.")
+
+            os.remove(f"{out}/{alg}_batch.zip")
 
         # X fits in memory: direct computation
         else:
             clf.fit(X, y)
+            with open(f"{out}/{alg}.zip", "wb") as fout_classifier:
+                pickle.dump(clf, fout_classifier, protocol=pickle.HIGHEST_PROTOCOL)
             print("Done.")
-
-        pickle.dump(clf, f"{out}/{alg}.zip", protocol=pickle.HIGHEST_PROTOCOL)
