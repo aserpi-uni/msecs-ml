606409116fdb70f2442fc55b61745b52f5f6d3c8
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2019-11-07 21:29:16 +0100

    Update main

--- hw1/__main__.py
+++ hw1/__main__.py
@@ -1,140 +1,70 @@
 import argparse
+import errno
 import json
 import os
-import pandas as pd
-import pickle
-from sklearn.preprocessing import MultiLabelBinarizer
 
-from hw1.datasets import family_dataframe, feature_dataframe, one_hot_encode
+from hw1.datasets import labels, test_dataset, train_datasets
 from hw1.evaluation import evaluate, print_heatmap
 from hw1.learning import learn
-from hw1.prediction import predict
+from hw1.prediction import consolidate, predict
 
 
-def family_classification(action, directory, directory_results, algorithms, keys):
-    try:
-        with open(f"{directory}/X_family.zip", "rb") as fin_X, \
-                open(f"{directory}/y_family.zip", "rb") as fin_y, \
-                open(f"{directory}/labels_family.zip", "rb") as fin_labels:
-            X = pickle.load(fin_X)
-            y = pickle.load(fin_y)
-            labels = pickle.load(fin_labels)
-
-    except (FileNotFoundError, TypeError):
-        # Get a DataFrame containing only malware in large families
-        families = family_dataframe(directory).groupby("family").filter(lambda f: len(f) > 20) \
-            .set_index("sha256").sort_index()
-
-        features = feature_dataframe(families.index.values, directory, keys)
-
-        # Preprocess datasets
-        labels = pd.unique(families.family)
-        X = one_hot_encode(MultiLabelBinarizer, features, features.columns)
-        y = families.family.ravel()
-
-        # Delete unnecessary data structures
-        del families
-        del features
-
-        with open(f"{directory}/X_family.zip", "wb") as fout_X:
-            pickle.dump(X, fout_X, protocol=pickle.HIGHEST_PROTOCOL)
-        with open(f"{directory}/y_family.zip", "wb") as fout_y:
-            pickle.dump(y, fout_y, protocol=pickle.HIGHEST_PROTOCOL)
-        with open(f"{directory}/labels_family.zip", "wb") as fout_labels:
-            pickle.dump(labels, fout_labels, protocol=pickle.HIGHEST_PROTOCOL)
-
-    if action == "evaluate":
-        scores, confusion_matrices = evaluate(X, y, algorithms)
-
-        # Save scores
-        with open(f"{directory_results}/scores", "w") as fout:
-            json.dump(scores, fout, indent=4, sort_keys=True)
-
-        # Print confusion matrices
-        for alg in scores:
-            print_heatmap(confusion_matrices[alg], alg, directory_results, labels)
-
-    elif action == "learn":
-        learn(X, y, algorithms, directory_results)
-
-    else:
-        raise KeyError
-
-
-def malware_classification(action, directory, directory_results, algorithms, keys):
-    try:
-        with open(f"{directory}/X_malware.zip", "rb") as fin_X, \
-                open(f"{directory}/y_malware.zip", "rb") as fin_y:
-            X = pickle.load(fin_X)
-            y = pickle.load(fin_y)
-
-    except (FileNotFoundError, TypeError):
-        malwares = family_dataframe(directory).set_index("sha256").sort_index()
-        features = feature_dataframe(sorted(os.listdir(f"{directory}/feature_vectors")), directory, keys)
-
-        # Preprocess datasets
-        malwares["family"] = "Malware"
-        malwares = malwares.rename(columns={"family": "malware"}).reindex(features.index, fill_value="Safe")
-        X = one_hot_encode(MultiLabelBinarizer, features, features.columns)
-        y = malwares.malware.ravel()
-
-        # Delete unnecessary data structures
-        del malwares
-        del features
-
-        with open(f"{directory}/X_malware.zip", "wb") as fout_X:
-            pickle.dump(X, fout_X, protocol=pickle.HIGHEST_PROTOCOL)
-        with open(f"{directory}/y_malware.zip", "wb") as fout_y:
-            pickle.dump(y, fout_y, protocol=pickle.HIGHEST_PROTOCOL)
-
-    if action == "evaluate":
-        scores, confusion_matrices = evaluate(X, y, algorithms)
-
-        # Save scores
-        with open(f"{directory_results}/scores", "w") as fout:
-            json.dump(scores, fout, indent=4, sort_keys=True)
-
-        # Print confusion matrices
-        for alg in scores:
-            print_heatmap(confusion_matrices[alg], alg, directory_results, ["Safe", "Malware"])
-
-    elif action == "learn":
-        learn(X, y, algorithms, directory_results)
-
-    else:
-        raise KeyError
-
-
-def main(action, classifier, drebin, out, algs=None, feats=None):
+def main(action, classifier, dataset_dir, out_dir, algs=None, cache=False, truncate=None):
     if not algs:
-        algs = ["bernoulli", "random_forest", "svc", "linear_svc"]
+        algs = ["naive_bayes", "random_forest", "svc", "linear_svc"]
 
-    if action == "predict":
-        predict(drebin, out, algs, feats)
-    elif action == "evaluate" or action == "learn":
-        if classifier == "malware":
-            malware_classification(action, drebin, out, algs, feats)
-        elif classifier == "family":
-            family_classification(action, drebin, out, algs, feats)
-        else:
-            raise KeyError
+    try:
+        os.makedirs(out_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    if action == "consolidate":
+        if len(algs) != 2:
+            raise ValueError("Specify two algorithms to consolidate"
+                             "(the first for the compiler, the second for the optimization level).")
+        consolidate(algs[0], algs[1], dataset_dir, out_dir)
+
+    elif action == "evaluate":
+        x, y = train_datasets(dataset_dir, classifier, cache=False, truncate=truncate)
+        scores, confusion_matrices = evaluate(x, y, algs, classes=labels(classifier))
+        with open(f"{out_dir}/scores_{classifier}.json", "w") as fout:
+            json.dump(scores, fout, indent=4, sort_keys=True)
+        for alg in scores:
+            print_heatmap(confusion_matrices[alg], classifier, alg, out_dir, labels=labels(classifier))
+
+    elif action == "learn":
+        x, y = train_datasets(dataset_dir, classifier, cache=cache, truncate=truncate)
+        learn(x, y, classifier, algs, out_dir)
+
+    elif action == "predict":
+        x = test_dataset(dataset_dir, truncate=truncate)
+        predict(x, classifier, algs, dataset_dir, out_dir)
+
     else:
-        raise KeyError
+        raise KeyError(f"Unknown action '{action}'")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Required arguments
-    parser.add_argument("--action")
-    parser.add_argument("--classifier")
-    parser.add_argument("--drebin")
-    parser.add_argument("--output")
+    parser.add_argument("action", choices=["consolidate", "evaluate", "learn", "predict"])
+    parser.add_argument("classifier", choices=["compiler", "opt"])
+    parser.add_argument("dataset-directory")
+    parser.add_argument("output-directory")
 
     # Optional arguments
-    parser.add_argument("--algorithms", nargs="*")
-    parser.add_argument("--features", nargs="*")
+    parser.add_argument("--algorithms", nargs="*", choices=["naive_bayes", "svc", "linear_svc", "random_forest"])
+    parser.add_argument("--no-cache", const=True, default=False, nargs="?")
+    parser.add_argument("--truncate", nargs="?", type=int)
 
     args = vars(parser.parse_args())
 
-    main(args["action"], args["classifier"], args["drebin"], args["output"], args["algorithms"], args["features"])
+    main(args["action"],
+         args["classifier"],
+         args["dataset-directory"],
+         args["output-directory"],
+         args["algorithms"],
+         not args["no_cache"],
+         args["truncate"])
