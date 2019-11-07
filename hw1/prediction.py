63e4c697bb580c98769c7e7649f62c75de23ea3a
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2019-11-07 21:29:36 +0100

    Update prediction.py

--- hw1/prediction.py
+++ hw1/prediction.py
@@ -1,16 +1,31 @@
-import os
+import numpy as np
 import pickle
-import pandas as pd
-from sklearn.preprocessing import MultiLabelBinarizer
-
-from hw1.datasets import feature_dataframe, one_hot_encode
 
 
-def predict(directory, directory_results, algorithms, keys):
-    features = feature_dataframe(sorted(os.listdir(f"{directory}/feature_vectors")), directory, keys)
-    X = one_hot_encode(MultiLabelBinarizer, features, features.columns)
-    del features
+def consolidate(compiler_algorithm, optimization_algorithm, dataset_directory, output_directory):
+    with open(f"{dataset_directory}/compiler_{compiler_algorithm}.csv") as fin_compiler, \
+            open(f"{dataset_directory}/opt_{optimization_algorithm}.csv") as fin_optimization, \
+            open(f"{output_directory}/results.csv", "w") as fout:
 
+        while True:
+            compiler = fin_compiler.readline()
+            opt = fin_optimization.readline()
+
+            if (compiler and not opt) or (not compiler and opt):
+                raise ValueError("The files have not the same number of lines")
+            if not compiler and not opt:
+                break
+
+            print(f"{compiler.strip()},{opt.strip()}", file=fout)
+
+
+def predict(x, classifier, algorithms, dataset_dir, results_dir):
     for alg in algorithms:
-        classifier = pickle.load(f"{directory}/classifiers/{alg}")
-        pd.Series(classifier.predict(X)).to_pickle(f"{directory_results}/{alg}.zip")
+        try:
+            with open(f"{dataset_dir}/classifiers/{classifier}_{alg}.zip", "rb") as fin:
+                pred = pickle.load(fin).predict(x)
+        except (FileNotFoundError, TypeError):
+            print(f"ERROR: No classifier '{alg}' for task '{classifier}'")
+            continue
+
+        np.savetxt(f"{results_dir}/{classifier}_{alg}.csv", pred, fmt="%s")
