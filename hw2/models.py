a1fef8a5f203860864c82d89b3e7022295a2d60e
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2019-12-08 19:10:21 +0100

    Even out fine-tuning models

--- hw2/models.py
+++ hw2/models.py
@@ -1,47 +1,42 @@
-from keras import models, layers
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout
 
 
 def inception(image_size, num_classes):
     from keras.applications import InceptionV3
     inception_conv = InceptionV3(weights="imagenet", include_top=False, input_shape=image_size.rgb_dimensions())
 
-    # Freeze all layers
-    for layer in inception_conv.layers:
-        layer.trainable = False
-
-    return add_bottom(inception_conv, num_classes)
+    return __fine_tuning_model(inception_conv, num_classes)
 
 
 def resnet50(image_size, num_classes):
     from keras.applications import ResNet50V2
     resnet_conv = ResNet50V2(weights="imagenet", include_top=False, input_shape=image_size.rgb_dimensions())
 
-    # Freeze all layers
-    for layer in resnet_conv.layers:
-        layer.trainable = False
-
-    return add_bottom(resnet_conv, num_classes)
+    return __fine_tuning_model(resnet_conv, num_classes)
 
 
 def vgg16(image_size, num_classes):
     from keras.applications import VGG16
     vgg_conv = VGG16(weights='imagenet', include_top=False, input_shape=image_size.rgb_dimensions())
 
+    return __fine_tuning_model(vgg_conv, num_classes)
+
+
+def __fine_tuning_model(original_model, num_classes):
+    model = Sequential(name=f"fine_tuning_{original_model.name}")
+
     # Freeze all layers except last 4
-    for layer in vgg_conv.layers[:-4]:
+    for layer in original_model.layers[:-4]:
         layer.trainable = False
 
-    return add_bottom(vgg_conv, num_classes)
-
-
-def add_bottom(original_model, num_classes):
-    model = models.Sequential()
+    # Add original network as the first layer
     model.add(original_model)
 
     # Add new layers
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1024, activation='relu'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(num_classes, activation='softmax'), )
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
 
     return model
