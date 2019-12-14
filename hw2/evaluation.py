b4e2c25a59aba6a444b4598a50bb4ff8d1a9ff20
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2019-12-14 17:18:42 +0100

    Improve metric plots

--- hw2/evaluation.py
+++ hw2/evaluation.py
@@ -1,9 +1,11 @@
+from math import ceil
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator
 import pandas as pd
 import seaborn as sns
 
 
-def plot_metrics(net, history):
+def plot_metrics(net, history, loss_ylim=None):
     metrics = pd.read_csv(history)
     metrics.index += 1
 
@@ -12,16 +14,22 @@ def plot_metrics(net, history):
                             "val_acc": "Validation accuracy",
                             "val_loss": "Validation loss"},
                    inplace=True)
+    x_formatter = ticker.MultipleLocator(math.ceil(len(metrics) / 20))
 
     plt.figure()
     ax = sns.lineplot(data=metrics[["Accuracy", "Validation accuracy"]], dashes=False, markers=True)
-    ax.set(xlabel="Epoch", ylabel='Accuracy')
+    ax.set(xlabel="Epoch", ylabel='Accuracy', ylim=(0, 1))
+    ax.xaxis.set_major_locator(x_formatter)
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
     ax.get_figure().savefig(history.parent / f"{net}_accuracy.pdf", format="pdf")
     plt.clf()
 
     plt.figure()
     ax = sns.lineplot(data=metrics[["Loss", "Validation loss"]], dashes=False, markers=True)
     ax.set(xlabel="Epoch", ylabel="Loss")
+    if loss_ylim:
+        ax.set(ylim=(0, loss_ylim))
+    ax.xaxis.set_major_locator(x_formatter)
     ax.get_figure().savefig(history.parent / f"{net}_loss.pdf", format="pdf")
     plt.clf()
 
